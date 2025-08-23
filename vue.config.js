c7a1bc469e64a7c77942d6d4606c87d4354d7c02
mrchazaaa <charlie.howlett@live.co.uk>
2025-08-23 21:59:28 +0100

    Add CI step to generate analytics report

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,9 @@
-const StatoscopeWebpackPlugin = require('@statoscope/webpack-plugin').default;
-
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devtool: "source-map",
-    plugins: [
-      ...(process.env.BUILD_STATS
-        ? [
-            new StatoscopeWebpackPlugin({
-              saveReportTo: 'dist/statoscope.html',
-              saveStatsTo: 'dist/statoscope-webpack-stats.json',
-              normalizeStats: true,
-              open: 'false'
-            }),
-          ]
-        : []),
-    ],
   },
   css: {
     extract: {
