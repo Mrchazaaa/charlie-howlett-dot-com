946136755dd76aafbd11cf6cfbea900ea18903a2
mrchazaaa <charlie.howlett@live.co.uk>
2025-08-26 23:17:19 +0100

    Fix LCP loading screen.

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -36,8 +36,9 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 9999;
+  z-index: 1000;
   overflow: hidden;
+  pointer-events: none;
 }
 
 .loading-content {
