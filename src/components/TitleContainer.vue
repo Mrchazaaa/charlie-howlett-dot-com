51adff8081d4c95cdf9e72bdf90843e91bfcadcf
mrchazaaa <charlie.howlett@live.co.uk>
2025-08-27 22:44:21 +0100

    preload-hero-images

--- src/components/TitleContainer.vue
+++ src/components/TitleContainer.vue
@@ -18,6 +18,7 @@
 
 <script>
   import {cloudsSketch, setTheme} from '../javascript/background.js';
+  import ImagePreloader from "../javascript/imagePreloader.js";
 
   export default {
     name: 'TitleContainerComponent',
@@ -25,6 +26,16 @@
       return {
         myp5: null
       }
+    },
+    created() {
+      const imageSources = [
+          require('@/assets/mountains.svg'),
+          require('@/assets/mountains-dark.svg'),
+          require("@/assets/moon.webp"),
+          require("@/assets/moon-dark.webp")
+      ];
+
+      ImagePreloader.preloadImages(imageSources);
     },
     mounted() {
       this.myp5 = cloudsSketch(document.getElementById('sketch'));
